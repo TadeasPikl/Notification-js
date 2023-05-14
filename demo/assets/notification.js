a4a82b16cd0c517d9c72f023a323eb4c25ff4c1f
TadeasPikl <tadeas+1@piklovi.cz>
2023-05-14 16:54:05 +0200

    stuff

--- demo/assets/notification.js
+++ demo/assets/notification.js
@@ -1,7 +1,30 @@
+export { sendNotification }
 
-
-window.addEventListener("DOMContentLoaded", (event) => {
-
-    
-
+window.addEventListener("DOMContentLoaded", () => {
+    notificationBox = document.querySelector("notification-box")
 });
+
+function sendNotification(message, type) {
+    const notif = document.createElement("div")
+
+    notif.classList.add("notification")
+    notif.textContent = message
+
+    switch (type) {
+        case 0:
+            break
+        case 1:
+            notif.classList.add("notification__success")
+            break
+        case 2:
+            notif.classList.add("notification__warning")
+            break
+        case 3:
+            notif.classList.add("notification__error")
+            break
+        default:
+            console.log("invalid notification type")
+    }
+
+    notificationBox.appendChild(notif)
+}
